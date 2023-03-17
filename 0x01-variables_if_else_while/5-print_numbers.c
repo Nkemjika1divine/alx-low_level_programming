13ced68c8455ab3322fcb59a0dece6c33fa482c9
Nkemjika1divine <cndivine@gmail.com>
2023-03-17 08:28:11 -0700

    C Variables commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This program prints all single digits of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
