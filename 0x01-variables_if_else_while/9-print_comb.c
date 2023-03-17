b8195f2026d1749faab70284eaaa6b0419c59353
Nkemjika1divine <cndivine@gmail.com>
2023-03-17 12:13:13 -0700

    C Variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,14 @@ int main(void)
 	int a;
 
 	for (a = 0 ; a < 10 ; a++)
+		putchar(' ');
 	{
 		if (a == 9)
 			putchar(a + '0');
 		else
-		{	
+		{
 			putchar(a + '0');
 			putchar(',');
-			putchar(' ');
 		}
 	}
 	return (0);
