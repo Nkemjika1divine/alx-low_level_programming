fb908b09eb483fca12341fc95e7c2314e1b72aef
Nkemjika1divine <cndivine@gmail.com>
2023-03-17 12:01:27 -0700

    C variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints all possible combination of single digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		if (a == 9)
+			putchar(a + '0');
+		else
+		{	
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
