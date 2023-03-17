06c336f15169f15834af93ee533d1926e9c7659d
Nkemjika1divine <cndivine@gmail.com>
2023-03-16 17:36:28 -0700

    C Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints a line # folllowed by a new line to the standard error
+ * main - a program that prints a line followed by new line to the std error
  *
  * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
 	
