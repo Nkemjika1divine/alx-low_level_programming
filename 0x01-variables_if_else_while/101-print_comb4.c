05fb3703ab2920269871f00af485a32fab63f3af
Nkemjika1divine <cndivine@gmail.com>
2023-03-17 13:21:53 -0700

    C Variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,13 +18,16 @@ int main(void)
 		{
 			for (c = 2 ; c < 10 ; c++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(c + '0');
-				if (a + b + c != 24)
+				if (a < b && b < c)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
