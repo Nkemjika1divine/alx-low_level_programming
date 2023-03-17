5d9d97fc1f9d78608141184b799e36c96421fae6
Nkemjika1divine <cndivine@gmail.com>
2023-03-17 13:13:11 -0700

    C Variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints all possible different combinations of 3  digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for (b = 1 ; b < 10 ; b++)
+		{
+			for (c = 2 ; c < 10 ; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a + b + c != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
