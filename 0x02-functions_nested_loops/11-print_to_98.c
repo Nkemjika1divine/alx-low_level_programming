da0b16b70c228614cba63329b3c88d16f4ebacf1
Nkemjika1divine <cndivine@gmail.com>
2023-03-21 16:56:53 -0700

    C Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,6 +18,8 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d, ", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -26,6 +28,8 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d, ", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -34,9 +38,11 @@ void print_to_98(int n)
 		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d, ", i);
+			if (i != 98)
+				printf(", ");
 		}
-		printf("\n");
+		printf("\n'");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
