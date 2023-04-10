021f0c57715b31f2c7603a2be8d2602bebbfd902
Nkemjika1divine <cndivine@gmail.com>
2023-04-10 12:45:45 -0700

    C argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
 	(void) argv; /*this line of code makes argv not to work*/
 
-	printf("%d \n", argc - 1); /* this line allows input to the integer argc*/
+	printf("%d\n", argc - 1); /* this line allows input to the integer argc*/
 
 	return (0);
 }
