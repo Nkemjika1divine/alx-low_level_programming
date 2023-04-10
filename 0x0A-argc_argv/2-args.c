db387ca444beb2064583e238dea0dee44188f505
Nkemjika1divine <cndivine@gmail.com>
2023-04-10 07:15:34 -0700

    C argc argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main - this proram prints the arguments it recieves in the command line
+ *
+ * @argc - number of arguments
+ *
+ * @argv - number of arrays
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s \n", argv[a]);
+	}
+	return (0);
+}
