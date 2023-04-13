c01922945381524fd80efe525fe0fe0042ac809f
Nkemjika1divine <cndivine@gmail.com>
2023-04-13 14:44:40 -0700

    C more malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate		
+ * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
@@ -17,5 +17,5 @@ void *malloc_checked(unsigned int b)
 
 	if (ptr == NULL)
 		exit(98);
-	return (ptr);		
+	return (ptr);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,27 +1,18 @@
 #include <stdlib.h>
-		
 #include "main.h"
-		
 
-		
 /**
-		
  * *string_nconcat - concatenates n bytes of a string to another string
-		
- * @s1: string to append to
-		
- * @s2: string to concatenate from
-		
- * @n: number of bytes from s2 to concatenate to s1
-		
  *
-		
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
  * Return: pointer to the resulting string
-		
  */
-		
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-		
+
 {
 	char *s;
 	unsigned int a = 0, b = 0, len1 = 0, len2 = 0;
@@ -51,5 +42,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (n >= len2 && a < (len1 + len2))
 		s[a++] = s2[b++];
 	s[a] = '\0';
-	return (s);		
+	return (s);
 }
