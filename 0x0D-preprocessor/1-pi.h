a189eda0ae9e6163db32fc02c174b10c565bbe2a
Nkemjika1divine <cndivine@gmail.com>
2023-04-17 10:28:58 -0700

    C Preprocessor

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,6 @@
 #ifndef PI_H
 #define PI_H
 
-#define PI 3.1415925655976676384
-/*this cde sets a definition for the preprocessor in 1-main.c file*/
+#define PI 3.14159256559
+/*this code sets a definition for the preprocessor in 1-main.c file*/
 #endif
