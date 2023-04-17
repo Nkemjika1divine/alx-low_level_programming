ce006ee92df08dc0c90d2042510fde7680768494
Nkemjika1divine <cndivine@gmail.com>
2023-04-17 10:08:25 -0700

    C Preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,5 +2,6 @@
 #define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
-/*this code sets a definition for the preprocessor #include in the 0-main.c file*/
+/*this code sets a definition for the preprocessor #include in the*/
+/*0-main.c file*/
 #endif
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,6 @@
 #ifndef PI_H
 #define PI_H
 
-#define PI 3.142
+#define PI 3.1415925655976676384
 /*this cde sets a definition for the preprocessor in 1-main.c file*/
 #endif
