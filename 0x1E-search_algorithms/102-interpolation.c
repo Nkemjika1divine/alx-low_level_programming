429eb792abcd71f4854f5ac655cb6db6196deeba
Nkemjika1divine <cndivine@gmail.com>
2024-01-19 00:35:31 -0800

    Search Algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -25,7 +25,7 @@ int interpolation_search(int *array, size_t size, int value)
 	}
 	else
 	{
-		printf("Value checked array[%ld] is out of rangei\n", pos);
+		printf("Value checked array[%ld] is out of range\n", pos);
 		return (-1);
 	}
 
