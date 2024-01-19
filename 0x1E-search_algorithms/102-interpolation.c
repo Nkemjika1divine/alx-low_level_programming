15925e9ae0ae67d3c4212928f17d910a728820fa
Nkemjika1divine <cndivine@gmail.com>
2024-01-19 02:48:57 -0800

    Search Algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,6 +1,53 @@
 #include "search_algos.h"
 
 /**
+ * search - goes deeper into the interpolation search
+ * @array: the array
+ * @low: the lowpoint
+ * @high: the highpoint
+ * @pos: the current position
+ * @size: the size of the array
+ * @value: the value to seek
+ *
+ * Return: index
+ */
+
+int search(int *array, int low, int high, int pos, int size, int value)
+{
+	double diff;
+	int new_pos;
+
+	if (low > high)
+		return (-1);
+	if (array[pos] > value)
+	{
+		diff = (double)((pos - 1) - low);
+		new_pos = low + (diff / (array[pos - 1] - array[low]) * (value - array[low]));
+		printf("Value checked array[%d] = [%d]\n", new_pos, array[new_pos]);
+		if (array[new_pos] == value)
+			return (new_pos);
+		else if (array[new_pos] < value)
+			return (search(array, pos + 1, high, new_pos, size, value));
+		else
+			return (search(array, low, pos - 1, new_pos, size, value));
+	}
+	else if (array[pos] < value)
+	{
+		diff = (double)(high - (pos + 1));
+		new_pos = (pos + 1) + (diff / (array[high] - array[pos + 1] ) * (value - array[pos + 1]));
+		printf("Value checked array[%d] = [%d]\n", new_pos, array[new_pos]);
+		if (array[new_pos] == value)
+			return (new_pos);
+		else if (array[new_pos] < value)
+			return (search(array, pos + 1, high, new_pos, size, value));
+		else if (array[new_pos] > value)
+			return (search(array, low, pos - 1, new_pos, size, value));
+	}
+
+	return (-1);
+}
+
+/**
  * interpolation_search - This function finds the index of an element
  * @array: the pointer to the array to seatrch
  * @size: the size of the array
@@ -22,6 +69,9 @@ int interpolation_search(int *array, size_t size, int value)
 	if ((pos <= (size_t)high) && (pos >= (size_t)low))
 	{
 		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] != value)
+			return (search(array, low, high, pos, (int)size, value));
+		return ((int)pos);
 	}
 	else
 	{
@@ -29,5 +79,5 @@ int interpolation_search(int *array, size_t size, int value)
 		return (-1);
 	}
 
-	return ((int)pos);
+	return (-1);
 }
